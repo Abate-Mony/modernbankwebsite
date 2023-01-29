d9d80f47bdb94ddd0873ca4db31834e0ff7e1ff8
mmanuel <bateemma14@gmail.com>
2023-01-29 19:50:32 +0100

    pushing to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Navbar, CTA, Brand, Feature, Article } from './components'
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 const App = () => {
   return (
-    <div className='App  color__danger'>
+    <div className='App  '>
       <div className="gradient__bg">
         <Navbar />
         <Header />
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,7 +1,53 @@
 import "./footer.css"
+import footerLogo from '../../assets/logo.svg'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section__padding ">
+      <h1 className="gradient__text">
+        Do you want to step on to the future before others
+      </h1>
+      <div className="btn"><span>Request Early access</span></div>
+
+      <div className="gpt3__footer-content">
+        <div className="gpt3__footer-content_header">
+ <img src={footerLogo} alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        
+        
+        <div className="gpt3__footer-content_text">      <ul>
+
+          <h4>Links</h4>
+          <li><a href="#">Ovevrons</a></li>
+          <li><a href="#">Social Media</a></li>
+          <li><a href="#">Counters</a></li>
+          <li><a href="#">Contacts</a></li>
+        </ul>
+        <ul>
+
+          <h4>Company</h4>
+          <li><a href="#">Terms and condition</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <ul>
+
+          <h4>Get in <i class="fas fa-scanner-touchscreen    "></i></h4>
+          <li><a href="#">Crecgterwoord k12 182 dk aknjkcb </a></li>
+          <li><a href="#">085-132567</a></li>
+          <li><a href="#">info@gmail.com</a></li>
+        </ul>
+        </div>
+
+      </div>
+      <div className="gpt3__footer-copyright">
+      <p>&copy; 202 GPt-3,  Al rights reserved.</p></div>
+
+    </div>
+
+
+
+
   )
 }
 
